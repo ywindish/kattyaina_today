2a9cc44369d91f91434bbc055b8d8a0610f3daec
yamako <yamako.windish@gmail.com>
2020-08-16 13:25:53 +0900

    Fix funayurei in peaceful world... modify search url

--- kattyaina_today.rb
+++ kattyaina_today.rb
@@ -5,12 +5,20 @@ require 'uri'
 require 'net/http'
 require 'date'
 require 'pp'
+require 'logger'
 require 'mastodon' # https://github.com/tootsuite/mastodon-api
 
 class KattyainaBot
 
-  def initialize(target_date = Date.today)
-    @target_date = target_date
+  def initialize(base_url, token, target_date=nil, dry_run=nil)
+    @logger = Logger.new(STDOUT)
+
+    @target_date = target_date || Date.today
+    @base_url = base_url
+    @token = token
+    @dry_run = dry_run
+
+    @logger.info("target date: #{@target_date}")
   end
 
   def run
@@ -21,19 +29,14 @@ class KattyainaBot
   end
 
   def get_schedules_json
-    # 真ん中の文字が変わることがある F, A など
-    query_date = @target_date.strftime('%YA%m')
+    # TODO think url generation algorizm F,A,D..
+    query_date = @target_date.strftime('%YD%m')
     url = %Q(https://search.nintendo.jp/nintendo_soft/search.json?opt_sshow=1&xopt_ssitu[]=sales_termination&fq=sodate_s%3A%5B#{query_date}%20TO%20*%5D&limit=300&page=1&sort=sodate%20asc%2Chards%20asc%2Csform_s%20asc%2Ctitle%20asc&opt_sche=1)
+    @logger.info("target url: #{url}")
+
     uri = URI.parse(url)
     json = Net::HTTP.get(uri)
-    # あとで検証する用に永続化しとく
-    File.open(__dir__ + '/tmp/result.json', 'w') do |f|
-      f.print json # backup
-    end
-    # TODO テスト用。rspec をかいておきたいところ
-    # json = File.open(__dir__ + '/spec/result.json') do |f|
-    #   f.read
-    # end
+
     JSON.parse(json)
   end
 
@@ -41,11 +44,9 @@ class KattyainaBot
     sdate_str = sprintf('%d.%d.%d', @target_date.year, @target_date.month, @target_date.day)
     items = []
     json['result']['items'].each do |item|
-      if item['sdate'] != sdate_str
+      if item['sdate'].to_s != sdate_str
         next
       end
-      # 長すぎてtootできないことがあるのでタイトルだけにして様子を見る。current_price は nil のこともあるので注意
-      #items.push sprintf('%s: %s %d円', item['maker'], item['title'], item['current_price'])
       items.push item['title']
     end
     items
@@ -58,18 +59,24 @@ class KattyainaBot
     else
       message = "本日発売のゲームはこちらです。カッチャイナー\n\n#{items.join("\n")}"
     end
-    message = message.slice(0,498) + '..' if message.size > 500 # 長過ぎるときは省略
-    pp message
+    message = message.slice(0,498) + '..' if message.size > 500 # too large
+    @logger.info("result message: #{message}")
+
+    message
   end
 
   def toot(message)
-    client = Mastodon::REST::Client.new(base_url: 'https://funayurei.windish.jp', bearer_token: ENV['MASTODON_TOKEN'])
+    return if @dry_run
+
+    client = Mastodon::REST::Client.new(base_url: @base_url, bearer_token: @token)
     client.create_status(message)
   end
 
 end
 
-KattyainaBot.new.run
-# TODO 引数を渡せるように。dry-run とかも指定できるとよい
-# KattyainaBot.new(Date.parse("2020.1.30")).run
-
+KattyainaBot.new(
+  ENV['MASTODON_URL'],
+  ENV['MASTODON_TOKEN'],
+  ENV['TARGET_DATE'],
+  ENV['DRY_RUN']
+).run
