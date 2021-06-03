de1d338cfb89ff01e0e478bd0d1c0ca9a1ac1683
yamako <yamako.windish@gmail.com>
2021-06-03 14:00:49 +0900

    Revice URL building algorizm, and if message is empty then no post

--- lib/kattyaina_bot.rb
+++ lib/kattyaina_bot.rb
@@ -37,8 +37,8 @@ class KattyainaBot
 
   def get_schedules_json
     # TODO think url generation algorizm F,A,D..
-    # F=12-2, D=9-11, A=?
-    query_date = @target_date.strftime('%YF%m')
+    # F=12-5, D=6,9-11, A=?
+    query_date = @target_date.strftime('%YD%m')
     url = %Q(https://search.nintendo.jp/nintendo_soft/search.json?opt_sshow=1&xopt_ssitu[]=sales_termination&fq=sodate_s%3A%5B#{query_date}%20TO%20*%5D&limit=300&page=1&sort=sodate%20asc%2Chards%20asc%2Csform_s%20asc%2Ctitle%20asc&opt_sche=1)
     @logger.info("target url: #{url}")
 
@@ -63,18 +63,19 @@ class KattyainaBot
   def build_toot_message(items)
     message = ''
     if items.empty?
-      message = '本日発売のゲームが見つかりませんでした。安心して積みゲーを崩せるかも。'
+      # message = '本日発売のゲームが見つかりませんでした。安心して積みゲーを崩せるかも。'
+      @logger.info("result message: empty.")
     else
       message = "本日発売のゲームはこちらです。カッチャイナー\n\n#{items.join("\n")}"
+      message = message.slice(0,498) + '..' if message.size > 500 # too large
+      @logger.info("result message: #{message}")
     end
-    message = message.slice(0,498) + '..' if message.size > 500 # too large
-    @logger.info("result message: #{message}")
 
     message
   end
 
   def toot(message)
-    return if @dry_run
+    return if @dry_run || message == ''
 
     client = Mastodon::REST::Client.new(base_url: @base_url, bearer_token: @token)
     client.create_status(message)
